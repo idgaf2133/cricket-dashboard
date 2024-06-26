ae395a95c28508465b10f994287f06b438c1555d
idgaf2133 <155026436+idgaf2133@users.noreply.github.com>
2024-06-26 21:01:30 +1000

    initial commit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Homepage.css'
+const Homepage =()=> {
+
+
+    return (
+        <div className = "home-page">
+
+
+            <h1>Welcome to the Cricket Dashboard</h1>
+            <p> Latest match updates and featured players will be displayed </p>
+        
+        
+        </div>
+
+
+
+
+    );
+
+
+};
+
+
+export default Homepage;
