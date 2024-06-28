6fd0b34beef574cc2f0aa0f66a51a2e9180de9c2
idgaf2133 <155026436+idgaf2133@users.noreply.github.com>
2024-06-28 16:04:11 +1000

    Chnages to the nav
    got it of the Iconbutton and added a box so that the items inside the box are displayed as flex

--- src/pages/components/NavigationBar.js
+++ src/pages/components/NavigationBar.js
@@ -1,6 +1,6 @@
        
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button,Box} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
      
@@ -8,30 +8,19 @@ const NavigationBar =()=> {
 
 
         return (
-        /*
-         <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/match-analysis">Match Analysis</Link></li>
-              <li><Link to="/player-statistics">Player Statistics</Link></li>
-              <li><Link to="/team-comparison">Team Comparison</Link></li>
-            </ul>
-          </nav>
-  
-          */
-
+       
         <AppBar position="static">
           <Toolbar>
-              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                  <MenuIcon />
-              </IconButton>
+             
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                   Cricket Dashboard
               </Typography>
+              <Box sx = {{flexGrow:1,  display: 'flex',justifyContent: 'center'}}>
               <Button color="inherit" component={Link} to="/">Home</Button>
               <Button color="inherit" component={Link} to="/match-analysis">Match Analysis</Button>
               <Button color="inherit" component={Link} to="/player-statistics">Player Statistics</Button>
               <Button color="inherit" component={Link} to="/team-comparison">Team Comparison</Button>
+              </Box>
           </Toolbar>
       </AppBar>
     
